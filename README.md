929e28deff458c21d16aee9cfab14c702bbabc27
Irina Ponamareva <ivponamareva@yandex.ru>
2021-05-27 18:29:02 +0300

    fix errors - 1 iteration

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
 # Проект 4: Место
 
-### Обзор
+Проект о путешествиях по России.
+Технологии: HTML, CSS, Javascript.
 
-* Figma
-* Картинки
-
-**Figma**
-
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
-
-**Картинки**
-
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
+Ссылка на GH-pages: https://iponamareva.github.io/mesto/index.html
---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+let popup = document.querySelector(".popup");
+let formElement = document.querySelector('.popup__form');
+let username = document.querySelector(".profile__username");
+let bio = document.querySelector(".profile__bio");
+
+let updUsername = document.querySelector('.popup__input_type_username');
+let updBio = document.querySelector('.popup__input_type_bio');
+
+let editButton = document.querySelector(".profile__edit-button");
+let closeButton = document.querySelector(".popup__close-button");
+
+function openPopup() {
+    updUsername.value = username.textContent;
+    updBio.value = bio.textContent;
+    popup.classList.add('popup_opened');
+    console.log('popup opened');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+    console.log('popup closed');
+}
+
+function submitFormHandler(evt) {
+    evt.preventDefault();
+    username.textContent = updUsername.value;
+    bio.textContent = updBio.value;
+}
+
+formElement.addEventListener('submit', submitFormHandler);
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
